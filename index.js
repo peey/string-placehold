f28520082a02beb5de1209fa980cf3f31910c720
Peeyush Kushwaha <peeyush.p97@gmail.com>
2015-06-30 23:18:19 +0530

    refactor: hold data in closure rather than an object

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var _ = require("lodash");
 
 var stringValueJoin = function (strings, values) {
+  //works as the _.zip correctly orders the values in front of their respective strings,
+  //then flatten falattens it out so now we have a big array of thing in order, ready to be joined.
+  //Compact is required because of unequal length of strings and values, zipping introduces some undefineds in the array.
   return _.compact(_.flatten(_.zip(strings, values))).join("");
 };
 
+//considers the array as an object with number keys to achieve the mapping. So it unintentionally will convert a ${"length"} placeholder to length of the array.
 var mapTokensToValues = function (tokens, map) {
   var values = [];
   _.each(tokens, function(token, index) {
@@ -12,43 +16,21 @@ var mapTokensToValues = function (tokens, map) {
   return values;
 };
 
-var Placeholder = function (strings, tokens) {
-  this.strings = strings;
-  this.tokens = tokens;
-};
-
-Placeholder.prototype.fill = function () {
-  var args = Array.prototype.slice.call(arguments, 0);
+var fill = function (strings, tokens, args) {
   var map = (_.isObject(args[0]) || _.isArray(args[0]))? args[0] : args;
-  var values = mapTokensToValues(this.tokens, map);
-  return stringValueJoin(this.strings, values);
+  var values = mapTokensToValues(tokens, map);
+  return stringValueJoin(strings, values);
 };
 
-var placeholdClosure = function (strings, tokens) {
-  var placeholder = new Placeholder(strings, tokens);
+var placeholderClosure = function (strings, tokens) {
   return function () {
-    return placeholder.fill.apply(placeholder, arguments);
+    var args = Array.prototype.slice.call(arguments, 0);
+    return fill(strings, tokens, args);
   };
 }
 
-/*
- * @example
- * 
- * var template = placehold `My ${"future"} template`
- * 
- * var result = template({future: "lovely"}); 
- * //result -> "My lovely template"
- * 
- * @example
- * 
- * var interpolate = placehold `My ${0} and ${1} template`
- * var result = interpolate("lovely", "neat");
- * //or
- * var result = interpolate(["lovely", "neat"]);
- * //result -> "My lovely and neat template"
- */
 var placehold = module.exports = function (strings) {
   var values = Array.prototype.slice.call(arguments, 1);
   var tokens = values;
-  return placeholdClosure(strings, tokens);
+  return placeholderClosure(strings, tokens);
 };
